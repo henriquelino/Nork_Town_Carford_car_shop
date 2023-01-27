a7eeb87dfcefc0a297093ba4871dbd96cea0a9dd
henrique lino <henrique.lino97@gmail.com>
2023-01-27 16:50:34 -0300

    added: entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+python manage.py makemigrations
+python manage.py migrate
+
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')" | python manage.py shell
+
+exec "$@"
