72842547caec90cc0c07e8acabb3be97616f85b4
henrique lino <henrique.lino97@gmail.com>
2023-01-27 21:15:23 -0300

    added: test with and without superuser

--- app/car_sales/tests/test_admin.py
+++ app/car_sales/tests/test_admin.py
@@ -2,8 +2,9 @@ from datetime import datetime
 from random import choice
 
 from car_sales.admin import SalesAdmin
-from car_sales.models import Car, CarColor, CarModel, Customer, Sales, User
+from car_sales.models import Car, CarColor, CarModel, Customer, Sales
 from django.contrib.admin.sites import AdminSite
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Permission
 from django.test import TestCase
 from loguru import logger
@@ -14,14 +15,12 @@ class MockRequest:
 
 
 class MockSuperUser:
-    is_superuser = False
+
+    def __init__(self, is_superuser: bool):
+        self.is_superuser = is_superuser
 
     def has_perm(self, perm, obj=None):
         return True
-
-
-request = MockRequest()
-request.user = MockSuperUser()
 
 
 class SaleTestCase(TestCase):
@@ -31,7 +30,7 @@ class SaleTestCase(TestCase):
         self.site = AdminSite()
 
         permission = Permission.objects.get(name='Can sell cars')
-        self.test_salesperson = User(username="test salesperson", email="test_user@email.com", is_staff=True)
+        self.test_salesperson = get_user_model()(username="test salesperson", email="test_user@email.com", is_staff=True)
         self.test_salesperson.save()
         self.test_salesperson.user_permissions.add(permission)
         self.test_salesperson.refresh_from_db()
@@ -53,7 +52,21 @@ class SaleTestCase(TestCase):
 
         return
 
+    def test_default_fields_superuser(self):
+
+        request = MockRequest()
+        request.user = MockSuperUser(is_superuser=True)
+        ma = SalesAdmin(Sales, self.site)
+        self.assertEqual(list(ma.get_form(request).base_fields), ['salesperson', 'customer', 'car', 'purchase_date'])
+
+        self.assertEqual(list(ma.get_fields(request)), ['salesperson', 'customer', 'car', 'purchase_date'])
+
+        self.assertEqual(list(ma.get_fields(request, self.sales_model)), ['salesperson', 'customer', 'car', 'purchase_date'])
+
     def test_default_fields(self):
+
+        request = MockRequest()
+        request.user = MockSuperUser(is_superuser=False)
         ma = SalesAdmin(Sales, self.site)
         self.assertEqual(list(ma.get_form(request).base_fields), ['salesperson', 'customer', 'car', 'purchase_date'])
 
