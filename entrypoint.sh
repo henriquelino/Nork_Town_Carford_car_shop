8cff8671bdcec1b5efd7af01a31c80f567a65c66
henrique lino <henrique.lino97@gmail.com>
2023-01-27 22:03:24 -0300

    changed: now create superuser only if it doesn't exists

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
-
+python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
 
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')" | python manage.py shell
-
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); 
+if not User.objects.filter(username='$SUPERUSER_USERNAME').exists():
+    User.objects.create_superuser('$SUPERUSER_USERNAME', '$SUPERUSER_EMAIL', '$SUPERUSER_PASSWORD')" | python manage.py shell
+    
 exec "$@"
